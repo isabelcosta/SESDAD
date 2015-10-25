43ca005d946e010852fd9282f9e400a195ac4c55
Isabel Costa <isabelcmdcosta@gmail.com>
2015-10-25 18:15:37 +0000

    added labels to PM GUI

--- PuppetMaster/PuppetMasterForm.Designer.cs
+++ PuppetMaster/PuppetMasterForm.Designer.cs
@@ -34,18 +34,20 @@
             this.bt_Script = new System.Windows.Forms.Button();
             this.tb_Log = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tb_Command
             // 
-            this.tb_Command.Location = new System.Drawing.Point(47, 67);
+            this.tb_Command.Location = new System.Drawing.Point(20, 55);
             this.tb_Command.Name = "tb_Command";
             this.tb_Command.Size = new System.Drawing.Size(367, 20);
             this.tb_Command.TabIndex = 0;
             // 
             // bt_Command
             // 
-            this.bt_Command.Location = new System.Drawing.Point(438, 64);
+            this.bt_Command.Location = new System.Drawing.Point(411, 52);
             this.bt_Command.Name = "bt_Command";
             this.bt_Command.Size = new System.Drawing.Size(75, 23);
             this.bt_Command.TabIndex = 1;
@@ -54,7 +56,7 @@
             // 
             // tb_Script
             // 
-            this.tb_Script.Location = new System.Drawing.Point(47, 132);
+            this.tb_Script.Location = new System.Drawing.Point(20, 120);
             this.tb_Script.Multiline = true;
             this.tb_Script.Name = "tb_Script";
             this.tb_Script.Size = new System.Drawing.Size(213, 248);
@@ -62,7 +64,7 @@
             // 
             // bt_Script
             // 
-            this.bt_Script.Location = new System.Drawing.Point(47, 400);
+            this.bt_Script.Location = new System.Drawing.Point(20, 384);
             this.bt_Script.Name = "bt_Script";
             this.bt_Script.Size = new System.Drawing.Size(75, 23);
             this.bt_Script.TabIndex = 3;
@@ -71,7 +73,7 @@
             // 
             // tb_Log
             // 
-            this.tb_Log.Location = new System.Drawing.Point(291, 132);
+            this.tb_Log.Location = new System.Drawing.Point(264, 120);
             this.tb_Log.Multiline = true;
             this.tb_Log.Name = "tb_Log";
             this.tb_Log.ReadOnly = true;
@@ -82,17 +84,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Location = new System.Drawing.Point(20, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Single Command";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Script Box";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "System Log";
+            // 
             // PuppetMasterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 452);
+            this.ClientSize = new System.Drawing.Size(512, 419);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tb_Log);
             this.Controls.Add(this.bt_Script);
@@ -114,6 +136,8 @@
         private System.Windows.Forms.Button bt_Script;
         private System.Windows.Forms.TextBox tb_Log;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
