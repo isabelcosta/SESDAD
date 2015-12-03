59991fce6da0a24921bc045325d1ffc67a5de7fb
Isabel Costa <isabelcmdcosta@gmail.com>
2015-12-03 07:31:35 +0000

    small design changes

--- PuppetMaster/PuppetMasterForm.Designer.cs
+++ PuppetMaster/PuppetMasterForm.Designer.cs
@@ -129,7 +129,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(853, 594);
+            this.ClientSize = new System.Drawing.Size(843, 594);
             this.Controls.Add(this.tb_ClearLog);
             this.Controls.Add(this.lb_Log);
             this.Controls.Add(this.lb_Script);
